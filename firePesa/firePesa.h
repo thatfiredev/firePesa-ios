021d9df17b441bf96746446cc9201d4599bc38da
Admin <admin@mac-admin.local>
2018-06-12 13:59:44 +0200

    Initial Commit

---
+++ firePesa/firePesa.h
@@ -0,0 +1,19 @@
+//
+//  firePesa.h
+//  firePesa
+//
+//  Created by Admin on 6/12/18.
+//  Copyright © 2018 Rosário Pereira Fernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for firePesa.
+FOUNDATION_EXPORT double firePesaVersionNumber;
+
+//! Project version string for firePesa.
+FOUNDATION_EXPORT const unsigned char firePesaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <firePesa/PublicHeader.h>
+
+
